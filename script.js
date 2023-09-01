e012a9922ca696d1226d24f74a024bd3e592bb0a
Parzival-creator <120360026+Parzival-creator@users.noreply.github.com>
2023-09-01 15:47:27 +0200

    Ajout de la localisation en temps que deuxième option

--- script.js
+++ script.js
@@ -158,3 +158,33 @@ function fillTable(data) {
         cell.textContent = data['time'][i];
     }
 }
+
+const buttonLocation = document.getElementById('geoloc');
+
+buttonLocation.addEventListener("click", function() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            meteoApi(latitude, longitude);
+        }, function(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    alert("L'utilisateur a refusé la demande de géolocalisation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert("Les informations de géolocalisation ne sont pas disponibles.");
+                    break;
+                case error.TIMEOUT:
+                    alert("La demande de géolocalisation a expiré.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    alert("Une erreur inconnue est survenue.");
+                    break;
+            }
+        });
+    } else {
+        alert("La géolocalisation n'est pas prise en charge par ce navigateur.");
+    }
+});
